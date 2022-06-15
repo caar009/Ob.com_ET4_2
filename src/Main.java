c7781ae562460d5ade6107e8dd0714697bfcd4c0
Cesar Aguilar <cesar.a.aguilar.ramirez@gmail.com>
2022-06-14 23:10:55 -0500

    Commit secundario

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         int numerolf;
-        int numeroWhile;
+        int numeroWhile = 0;
         String estacion;
         Scanner leer = new Scanner(System.in);
 
-        System.out.print("Captura un valor: ");
+        System.out.print("Captura un valor numerico: ");
         numerolf = leer.nextInt();
 
         if ( numerolf > 0){
@@ -21,17 +21,18 @@ public class Main {
             System.out.println("El valor es negativo");
         }
 
-        numeroWhile = 0;
-
+        System.out.println("Ejecucion de Bucle While");
         while ( numeroWhile < 3){
             System.out.println(numeroWhile);
             numeroWhile++;
         }
 
+        System.out.println("Ejecucion de Bucle Do-While");
         do {
             System.out.println(numeroWhile);
         } while(numeroWhile < 3);
 
+        System.out.println("Ejecucion de Bucle For");
         for(int numeroFor = 0;
             numeroFor <= 3;
             numeroFor ++) {
@@ -51,7 +52,12 @@ public class Main {
             }
         }
 
-        System.out.println("Tecleaste: "+estacion);
+        switch (estacion) {
+            case "primavera" -> System.out.println("Te encuentras en la estacion primavera");
+            case "verano" -> System.out.println("Te encuentras en la estacion verano");
+            case "otoño" -> System.out.println("Te encuentras en la estacion otoño");
+            case "invierno" -> System.out.println("Te encuentras en la estacion invierno");
+        }
     }
 
 }
