97218ee8a6f1d7c7b3b14f1d5f037e1b4d7d87e0
Cesar Aguilar <cesar.a.aguilar.ramirez@gmail.com>
2022-06-14 22:18:10 -0500

    Commit inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numerolf;
+        int numeroWhile;
+        String estacion;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.print("Captura un valor: ");
+        numerolf = leer.nextInt();
+
+        if ( numerolf > 0){
+            System.out.println("El valor es positivo");
+        }
+        if ( numerolf == 0){
+            System.out.println("El valor es igual a 0");
+        }
+        if ( numerolf < 0){
+            System.out.println("El valor es negativo");
+        }
+
+        numeroWhile = 0;
+
+        while ( numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+
+        do {
+            System.out.println(numeroWhile);
+        } while(numeroWhile < 3);
+
+        for(int numeroFor = 0;
+            numeroFor <= 3;
+            numeroFor ++) {
+            System.out.println(numeroFor);
+        }
+
+        System.out.print("Teclea la estacion: ");
+        estacion = leer.next();
+
+        if ( !estacion.equals("primavera")){
+            if (!estacion.equals("verano")) {
+                if (!estacion.equals("otoño")) {
+                    if (!estacion.equals("invierno")) {
+                        estacion = "primavera";
+                    }
+                }
+            }
+        }
+
+        System.out.println("Tecleaste: "+estacion);
     }
+
 }
